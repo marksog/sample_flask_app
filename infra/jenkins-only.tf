9529389f6f2ec8b7469bb21bb9396470de2bab7b
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2025-06-25 16:29:03 -0400

    clean up

--- infra/jenkins-only.tf
+++ infra/jenkins-only.tf
@@ -1,12 +1,14 @@
-module "jenkins_first" {
-  source         = "./modules/jenkins"
-  env            = "dev"
-  vpc_id         = module.network.vpc_id
-  public_subnets = module.network.public_subnets
-  key_name       = var.key_name
+# module "jenkins_first" {
+#   source         = "./modules/jenkins"
+#   env            = "dev"
+#   vpc_id         = module.network.vpc_id
+#   public_subnets = module.network.public_subnets
+#   key_name       = var.key_name
 
-  bastion_sg_id = module.bastion.security_group_id
-  cluster_name = module.eks.cluster_name
-  # disable other modules 
+#   bastion_sg_id = module.bastion.security_group_id
+#   cluster_name = module.eks.cluster_name
+#   # disable other modules 
   
-}
+# }
+
+# DID NOT EVENTUALLY USE THIS FILE
