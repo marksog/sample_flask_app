c15327da5423c5a59d4652b75ca88263e10a5cc4
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2025-06-25 09:44:02 -0400

    updating eks

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -12,43 +12,45 @@ module "eks" {
   eks_managed_node_groups = {}
 
 }
-  resource "aws_eks_node_group" "public_node_group" {
-    cluster_name    = var.cluster_name
-    node_group_name = "${var.env}-public-node-group"
-    node_role_arn   = aws_iam_role.nodes.arn
-    subnet_ids      = var.public_subnets
-    scaling_config {
-      desired_size = 2
-      max_size     = 5
-      min_size     = 1
-    }
-    ami_type         = "AL2_x86_64"
-    instance_types   = ["t3.medium"]
-    capacity_type    = "ON_DEMAND"
-    disk_size        = 20
+resource "aws_eks_node_group" "public_node_group" {
+  cluster_name    = module.eks.cluster_name
+  node_group_name = "${var.env}-public-node-group"
+  node_role_arn   = aws_iam_role.nodes.arn
+  subnet_ids      = var.public_subnets
 
-    labels = {
-      nodegroup   = "public"
-      environment = var.env
-    }
-
-    taint {
-      key    = "public"
-      value  = "true"
-      effect = "NO_SCHEDULE"
-    }
-
-    remote_access {
-      ec2_ssh_key = var.key_name
-    }
-
-    lifecycle {
-      create_before_destroy = true
-      ignore_changes        = [scaling_config[0].desired_size]
-    }
-
-    depends_on = [module.eks]
+  scaling_config {
+    desired_size = 2
+    max_size     = 5
+    min_size     = 1
   }
+
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.medium"]
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+
+  labels = {
+    nodegroup   = "public"
+    environment = var.env
+  }
+
+  taint {
+    key    = "public"
+    value  = "true"
+    effect = "NO_SCHEDULE"
+  }
+
+  remote_access {
+    ec2_ssh_key = var.key_name
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [scaling_config[0].desired_size]
+  }
+
+  depends_on = [module.eks]
+}
 
     resource "aws_eks_node_group" "private_node_group" {
         cluster_name    = var.cluster_name
